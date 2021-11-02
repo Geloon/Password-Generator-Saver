b111e40c244d78b7fc6eed630074c3a58bad601a
Pol <pacadi92@gmail.com>
2021-11-02 14:55:37 +0100

    bug fix
    now the password generator clears the last generated password (they stacked each other, creating a long string)

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def password_generator():
+    password_entry.delete(0, 'end')
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
